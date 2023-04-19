9cbea18dadec2740775dcd76023422c619492764
Kettengeschwindigkeit <666damned999@mail.ru>
2023-04-20 00:44:46 +0300

    getServerSideProps

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,10 @@
-import { FC, PropsWithChildren } from "react"
-import { IMeta } from "../seo/meta.interface"
-import Header from "./header/Header"
-import Meta from "../seo/Meta"
+import { FC, PropsWithChildren } from "react";
+import dynamic from "next/dynamic";
+import { IMeta } from "../seo/meta.interface";
+import Meta from "../seo/Meta";
+import Header from "./header/Header";
+
+const DynamicFooter = dynamic(() => import("./Footer"), { ssr: false })
 
 const Layout: FC<PropsWithChildren<IMeta>> = ({ children, title, description }) => {
     return (
@@ -10,8 +13,10 @@ const Layout: FC<PropsWithChildren<IMeta>> = ({ children, title, description }) 
             <main>
                 {children}
             </main>
+            <DynamicFooter />
         </Meta>
     )
 }
 
-export default Layout
+export default Layout;
+    
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
-import Home from '@/components/screens/home/Home'
+import { GetServerSideProps, NextPage } from "next";
+import { ICarData } from "@/interfaces/car.interface";
+import { CarService } from "@/services/car.service";
+import Home from "@/components/screens/home/Home";
 
-export default function HomePage() {
-  return <Home />
+const HomePage: NextPage<ICarData> = ({ cars }) => {
+  return <Home cars={cars} />
 }
+
+export const getServerSideProps: GetServerSideProps<ICarData> = async () => {
+  const cars = await CarService.getAll()
+
+  return {
+    props: { cars }
+  }
+}
+
+export default HomePage;
