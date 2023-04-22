a90156d28cdcd20b778410a6085e197a6a1651f1
Kettengeschwindigkeit <666damned999@mail.ru>
2023-04-22 17:11:48 +0300

    next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL
+  },
+  basePath: "/red",
+  async rewrites() {
+    return [
+      {
+        source: "/images/:path*",
+        destination: "http://localhost:4200/images/:path*"
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: "/car/1",
+        destination: "/car/2",
+        permanent: true
+      }
+    ]
+  },
   images: {
     domains: ['localhost'],
   },
